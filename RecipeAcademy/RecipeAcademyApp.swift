3ea936d770b8a15e73c1b239efef89e95a1393b1
JTC <jt.cruzado.swe@gmail.com>
2022-07-05 14:10:33 -0400

    Initial Commit

---
+++ RecipeAcademy/RecipeAcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAcademyApp.swift
+//  RecipeAcademy
+//
+//  Created by Jeremy T. Cruzado on 7/5/22.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

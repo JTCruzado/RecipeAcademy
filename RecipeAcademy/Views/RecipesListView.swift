6276c991b746690276d5182b29e1beffc9687125
JTC <jt.cruzado.swe@gmail.com>
2022-07-05 20:11:11 -0400

    Added two new views to display recipe data and added in a color scheme.

--- RecipeAcademy/Views/RecipesListView.swift
+++ RecipeAcademy/Views/RecipesListView.swift
@@ -6,19 +6,26 @@
 //
 
 import SwiftUI
-
+ 
 struct RecipesListView: View {
   @StateObject var recipeData = RecipeData()
+ 
+  private let listBackgroundColor = AppColor.background
+  private let listTextColor = AppColor.foreground
+ 
   var body: some View {
     List {
-      ForEach(recipeData.recipes) { recipe in
-        Text(recipe.mainInformation.name)
+      ForEach(recipes) { recipe in
+        NavigationLink(recipe.mainInformation.name,
+                       destination: RecipeDetailView(recipe: recipe))
       }
+      .listRowBackground(listBackgroundColor)
+      .foregroundColor(listTextColor)
     }
-    .navigationTitle("All Recipes")
+    .navigationTitle(navigationTitle)
   }
 }
-
+ 
 extension RecipesListView {
   var recipes: [Recipe] {
     recipeData.recipes

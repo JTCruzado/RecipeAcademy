49d0eccc2cd144ec9de786888e715e0a6493b0ae
JTC <jt.cruzado.swe@gmail.com>
2022-07-10 10:51:30 -0400

    Built the recipe category grid view to display the food ,connected the two views with navigation link so the user can navigate through the app.

--- RecipeAcademy/Views/RecipesListView.swift
+++ RecipeAcademy/Views/RecipesListView.swift
@@ -6,40 +6,41 @@
 //
 
 import SwiftUI
- 
+
 struct RecipesListView: View {
-  @StateObject var recipeData = RecipeData()
- 
-  private let listBackgroundColor = AppColor.background
-  private let listTextColor = AppColor.foreground
- 
-  var body: some View {
-    List {
-      ForEach(recipes) { recipe in
-        NavigationLink(recipe.mainInformation.name,
-                       destination: RecipeDetailView(recipe: recipe))
-      }
-      .listRowBackground(listBackgroundColor)
-      .foregroundColor(listTextColor)
+    @EnvironmentObject private var recipeData: RecipeData
+    let category: MainInformation.Category
+    
+    private let listBackgroundColor = AppColor.background
+    private let listTextColor = AppColor.foreground
+    
+    var body: some View {
+        List {
+            ForEach(recipes) { recipe in
+                NavigationLink(recipe.mainInformation.name,
+                               destination: RecipeDetailView(recipe: recipe))
+            }
+            .listRowBackground(listBackgroundColor)
+            .foregroundColor(listTextColor)
+        }
+        .navigationTitle(navigationTitle)
     }
-    .navigationTitle(navigationTitle)
-  }
 }
- 
+
 extension RecipesListView {
-  var recipes: [Recipe] {
-    recipeData.recipes
-  }
- 
-  var navigationTitle: String {
-    "All Recipes"
-  }
-}
- 
-struct RecipesListView_Previews: PreviewProvider {
-  static var previews: some View {
-    NavigationView {
-      RecipesListView()
+    private var recipes: [Recipe] {
+        recipeData.recipes(for: category)
     }
-  }
+    private var navigationTitle: String {
+        "\(category.rawValue) Recipes"
+    }
+}
+
+struct RecipesListView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            RecipesListView(category: .breakfast)
+                .environmentObject(RecipeData())
+        }
+    }
 }

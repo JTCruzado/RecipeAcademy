0898df4c62c98924ee89191d410e78a2ddd14346
JTC <jt.cruzado.swe@gmail.com>
2022-08-04 13:21:18 -0400

    add form to create new recipes

--- RecipeAcademy/Views/ModifyRecipes/ModifyRecipeView.swift
+++ RecipeAcademy/Views/ModifyRecipes/ModifyRecipeView.swift
@@ -6,28 +6,44 @@
 //
 
 import SwiftUI
- 
+
 struct ModifyRecipeView: View {
-  @Binding var recipe: Recipe
- 
-  var body: some View {
-    Button("Fill in the recipe with test data.") {
-      recipe.mainInformation = MainInformation(name: "test",
-                                               description: "test",
-                                               author: "test",
-                                               category: .breakfast)
-      recipe.directions = [Direction(description: "test",
-                                     isOptional: false)]
-      recipe.ingredients = [Ingredient(name: "test",
-                                       quantity: 1.0,
-                                       unit: .none)]
+    @Binding var recipe: Recipe
+    
+    @State private var selection = Selection.main
+    
+    var body: some View {
+        VStack{
+            Picker("Select Recipe Component", selection: $selection) {
+                Text("Main Info").tag(Selection.main)
+                Text("Ingredients").tag(Selection.ingredients)
+                Text("Directions").tag(Selection.directions)
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            .padding()
+            switch selection {
+            case .main:
+              ModifyMainInformationView(mainInformation: $recipe.mainInformation)
+            case .ingredients:
+              Text("Ingredients Editor")
+            case .directions:
+              Text("Directions Editor")
+            }
+            Spacer()
+        }
     }
-  }
+    enum Selection {
+        case main
+        case ingredients
+        case directions
+    }
+
 }
- 
+
+
 struct ModifyRecipeView_Previews: PreviewProvider {
-  @State static var recipe = Recipe()
-  static var previews: some View {
-    ModifyRecipeView(recipe: $recipe)
-  }
+    @State static var recipe = Recipe()
+    static var previews: some View {
+        ModifyRecipeView(recipe: $recipe)
+    }
 }
